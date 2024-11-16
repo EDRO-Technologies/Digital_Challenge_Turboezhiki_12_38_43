2b6be1e72b6117d229ea027674d5b82255bb5aa8
Tosking <tosking@duck.com>
2024-11-16 23:34:23 +0500

    AAaaaAAaaAaaaaAAA

--- Ugra_IT_Back/src/routes/public/events.ts
+++ Ugra_IT_Back/src/routes/public/events.ts
@@ -7,6 +7,7 @@ import inputHandler from "../../handlers/inputHandler";
 const prisma = new PrismaClient();
 
 eventsRouter.post("/event", async (req, res) => {
+    const skillData = req.body.skilIds.map((id: number) => ({ skillId: id }));
     const userRow = await prisma.user.findFirst({
         where: {
             id: req.tokenID
@@ -25,13 +26,37 @@ eventsRouter.post("/event", async (req, res) => {
         const result = await prisma.event.create({
             data: req.body
         })
-        res.send(result)
+        const skillsRows = await prisma.skill_to_event.createMany({
+            data: skillData,
+        })
+        res.send({
+            ...skillData,
+            ...result
+        })
     } catch (err) {
         res.sendStatus(400)
         return
     }
 })
 
-eventsRouter.get("/event")
+eventsRouter.get("/event", async (req, res) => {
+    const page = parseInt(req.query.page as string) || 0
+    const searchString = inputHandler.defaultHandler(req.query.searchString as string) || ""
+
+    const pageSize = 10
+
+    const eventRows = await prisma.event.findMany({
+        where: {
+            name: {
+                contains: searchString,
+                mode: 'insensitive'
+            }
+        },
+        skip: page * pageSize,
+        take: pageSize
+    })
+
+    res.send(eventRows)
+})
 
 export default inputHandler
