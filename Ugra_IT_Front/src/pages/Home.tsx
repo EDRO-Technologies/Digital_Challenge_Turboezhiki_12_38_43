9b06f6ab127a4649d7d53dbcf272c73636151224
ZkunCry <aksentev.03@mail.ru>
2024-11-17 11:27:40 +0500

    added main page

--- Ugra_IT_Front/src/pages/Home.tsx
+++ Ugra_IT_Front/src/pages/Home.tsx
@@ -1,89 +1,348 @@
+import Header from "@/components/widgets/header";
+import React from "react";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import Arrow from "../assets/Arrow.svg";
+import Template1 from "../assets/Template1.jpg";
+import Template2 from "../assets/Template2.jpg";
+import Template3 from "../assets/Template3.jpg";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   return (
     <>
-      <header className="w-full py-[18px]">
-        <div className="container px-[16px] mx-auto">
-          <div className="flex items-center justify-between inner">
-            <div className="flex gap-[30px]">
-              <div className="logo"></div>
-              <nav className="flex gap-[30px]">
-                <Link to={"/tests"}>Тесты</Link>
-                <Link to={"/events"}>Мероприятия</Link>
-                <Link to={"/toys"}>Плюшки</Link>
-                <Link to={"/grants"}>Гранты</Link>
-                <Link to={"/courses"}>Курсы</Link>
-              </nav>
-            </div>
-            <div className="w-[44px] aspect-square bg-gray-500 rounded-full"></div>
+      <Header />
+
+      <div className="flex items-center justify-center bg-main-gradient min-h-[646px]">
+        <div className="container mx-auto px-[16px] h-full">
+          <div className="flex relative h-full items-center ">
+            <h1 className="relative text-[70px] uppercase text-white font-bold ">
+              Твоя траектория <br /> успеха начинается <br /> здесь
+              <span className="absolute bottom-[50px] right-[-300px] text-[28px] text-white">
+                Единая система для профисионального роста
+              </span>
+            </h1>
           </div>
         </div>
-      </header>
-      <div className="flex flex-col">
-        <div className="w-full mt-[84px] mb-[61px]">
-          <div className="container px-[16px] mx-auto">
+      </div>
+      <div className="flex mt-[156px] mb-[100px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex flex-col gap-[68px]">
+            <h1 className="uppercase text-[80px] font-bold">мы поможем</h1>
+            <div className="flex items-start gap-[16px]">
+              <div
+                className="border flex-grow-[1] flex-shrink-0 basis-[33.333%] p-[30px]"
+                style={{
+                  borderImageSource:
+                    "linear-gradient(121.72deg, #1CBCEC 8%, #09147A 76.01%)",
+                  borderImageSlice: 1,
+                  borderRadius: "35px",
+                }}
+              >
+                Выстроить карьерный рост
+              </div>
+              <div
+                className="border flex-grow-[1] flex-shrink-0 basis-[33.333%] p-[30px]"
+                style={{
+                  borderImageSource:
+                    "linear-gradient(121.72deg, #1CBCEC 8%, #09147A 76.01%)",
+                  borderImageSlice: 1,
+                  borderRadius: "35px",
+                }}
+              >
+                Получить поддержку, экспертную оценку и доступ к мероприятиям
+              </div>
+              <div
+                className="border flex-grow-[1] flex-shrink-0 basis-[33.333%] p-[30px]"
+                style={{
+                  borderImageSource:
+                    "linear-gradient(121.72deg, #1CBCEC 8%, #09147A 76.01%)",
+                  borderImageSlice: 1,
+                  borderRadius: "35px",
+                }}
+              >
+                Запустить стартап с инновационным потенциалом и стать резидентом
+                Технопарка Югры
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex mb-[350px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex flex-col gap-[76px]">
+            <div className="text flex-col flex-1">
+              <h1 className="uppercase text-[80px]">Что такое</h1>
+              <h1 className="uppercase text-[80px] text-right">«На взлёт!»?</h1>
+            </div>
+            <div className="flex self-center items-start justify-center flex-col gap-[15px] max-[834px]">
+              <span>«На взлёт!» – это ваш личный ИИ-навигатор в мире ИТ.</span>
+              <span>
+                Он знает, какие навыки сейчас ценятся, какие знания помогут
+                стать востребованным, и как быстрее достичь цели.
+              </span>
+              <span>
+                Создайте свою уникальную траекторию успеха – от первых шагов до
+                громких побед!
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex mb-[242px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex flex-col">
+            <h1 className="text-[70px]"> Мероприятия</h1>
             <div className="flex flex-col gap-[20px]">
-              <h1 className="text-[70px] text-[#273145] font-bold">
-                Учись сегодня — создавай завтра!
-              </h1>
-              <div className="flex flex-col self-end">
-                <span>
-                  Онлайн и офлайн курсы для программистов всех уровней.{" "}
+              <div className="flex flex-1 gap-[31px] card bg-white border rounded-[45px] p-[13px]">
+                <img src={Template1} alt="template1" />
+                <div className="flex flex-col w-full">
+                  <div className="flex gap-[20px]">
+                    <div className="bg-[#F4F5FF]  rounded-[30px] p-[10px]">
+                      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        До начала осталось 3 дня 12 часов 45 минут
+                      </h1>
+                    </div>
+                    <div className="bg-[#F4F5FF] rounded-[30px] p-[10px]">
+                      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        Хакатон
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="flex flex-col flex-1 gap-[30px]  items-start">
+                    <h1 className="text-[42px] ">Digital challenge</h1>
+                    <p className="text-left text-[22px]">
+                      <span className="block ">Дата: 16-17 ноября</span>
+                      <span className="block">Время: 10:00</span>
+                      <span className="block">Место: г. Сургут, школа 21</span>
+                    </p>
+                    <Button
+                      variant={"outline"}
+                      size={"default"}
+                      className="self-end text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                    >
+                      Участвовать
+                      <img src={Arrow} alt="#" />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-1 gap-[31px] card bg-white border rounded-[45px] p-[13px]">
+                <img src={Template2} alt="template1" />
+                <div className="flex flex-col w-full">
+                  <div className="flex gap-[20px]">
+                    <div className="bg-[#F4F5FF]  rounded-[30px] p-[10px]">
+                      <h1 className=" bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        До начала осталось 7 дней 12 часов 45 минут
+                      </h1>
+                    </div>
+                    <div className="bg-[#F4F5FF] rounded-[30px] p-[10px]">
+                      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        Хакатон
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="flex flex-col flex-1 gap-[30px]  items-start">
+                    <h1 className="text-[42px] ">Хакатон 2024</h1>
+                    <p className="text-left text-[22px]">
+                      <span className="block">Дата: 30 ноября - 1 декабря</span>
+                      <span className="block">Время: 10:00</span>
+                      <span className="block">Место: г. Сургут, школа 21</span>
+                    </p>
+                    <Button
+                      variant={"outline"}
+                      size={"default"}
+                      className="self-end text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                    >
+                      Участвовать
+                      <img src={Arrow} alt="#" />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-1 gap-[31px] card bg-white border rounded-[45px] p-[13px]">
+                <img src={Template3} alt="template1" />
+                <div className="flex flex-col w-full">
+                  <div className="flex gap-[20px]">
+                    <div className="bg-[#F4F5FF]  rounded-[30px] p-[10px]">
+                      <h1 className=" bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        До начала осталось 12 дней 12 часов 30 минут
+                      </h1>
+                    </div>
+                    <div className="bg-[#F4F5FF] rounded-[30px] p-[10px]">
+                      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+                        Форум
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="flex flex-col flex-1 gap-[30px]  items-start">
+                    <h1 className="text-[42px] ">IT-форум</h1>
+                    <p className="text-left text-[22px]">
+                      <span className="block">Дата: 11 декабря</span>
+                      <span className="block">Время: 12:00</span>
+                      <span className="block">Место: г. Сургут, школа 21</span>
+                    </p>
+                    <Button
+                      variant={"outline"}
+                      size={"default"}
+                      className="self-end text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                    >
+                      Участвовать
+                      <img src={Arrow} alt="#" />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h1 className="text-[22px] text-center cursor-pointer mt-[62px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+              Смотреть больше
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="container mx-auto px-[16px]">
+          <div className="flex flex-col gap-[40px]">
+            <h1 className="uppercase text-[80px] font-bold">тесты</h1>
+            <div className="flex gap-[20px]">
+              <div className="flex flex-col flex-1 p-[30px] gap-[10px] bg-white rounded-[45px]">
+                <span className="text-right">20 вопросов</span>
+                <h1 className="text-[26px]">
+                  Введение в аналитическое мышление
+                </h1>
+                <span className="text-[16px]">
+                  Тест на базовые принципы и подходы в аналитике
                 </span>
-                <span>Выбирай направление и развивай навыки!</span>
+                <Button
+                  variant={"outline"}
+                  size={"default"}
+                  className="self-end mt-[30px] text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                >
+                  Пройти тестирование
+                  <img src={Arrow} alt="#" />
+                </Button>
+              </div>
+              <div className="flex flex-col flex-1 p-[30px] gap-[10px] bg-white rounded-[45px]">
+                <span className="text-right">15 вопросов</span>
+                <h1 className="text-[26px]">Основы анализа данных</h1>
+                <span className="text-[16px]">
+                  Проверка начальных навыков работы с данными
+                </span>
+                <Button
+                  variant={"outline"}
+                  size={"default"}
+                  className="self-end mt-[30px] text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                >
+                  Пройти тестирование
+                  <img src={Arrow} alt="#" />
+                </Button>
+              </div>
+            </div>
+            <h1 className="text-[22px] text-center cursor-pointer mt-[62px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+              Смотреть больше
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="container mx-auto px-[16px]">
+          <div className="flex flex-col gap-[40px]">
+            <h1 className="uppercase text-[80px] font-bold">курсы</h1>
+            <div className="flex gap-[40px]">
+              <div className="flex flex-col flex-1  p-[30px] gap-[40px] bg-white rounded-[45px]">
+                <Button
+                  variant={"outline"}
+                  size={"default"}
+                  className="self-end mt-[30px] text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                >
+                  Учиться
+                  <img src={Arrow} alt="#" />
+                </Button>
+                <div className="flex flex-col gap-[57px]">
+                  <h1 className="text-[34px]">Программирование с нуля</h1>
+                  <ul className="flex flex-col gap-[20px] pl-[20px] list-disc ">
+                    <li>
+                      Изучите основы программирования и создайте свои первые
+                      приложения
+                    </li>
+                    <li>
+                      Практические задания помогут закрепить знания с первых
+                      занятий
+                    </li>
+                    <li>Идеально подходит для новичков без опыта в IT</li>
+                  </ul>
+                </div>
+                <div className="flex justify-between">
+                  <div className=" bg-[#F4F5FF] p-[15px] rounded-[30px] ">
+                    Срок обучения: 12 месяцев
+                  </div>
+                  <h1>Старт 3 декабря</h1>
+                </div>
+              </div>
+              <div className="flex flex-col flex-1  p-[30px] gap-[40px] bg-white rounded-[45px]">
+                <Button
+                  variant={"outline"}
+                  size={"default"}
+                  className="self-end mt-[30px] text-[20px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]"
+                >
+                  Учиться
+                  <img src={Arrow} alt="#" />
+                </Button>
+                <div className="flex flex-col gap-[57px]">
+                  <h1 className="text-[34px]">Веб-разработка</h1>
+                  <ul className="flex flex-col gap-[20px] pl-[20px] list-disc ">
+                    <li>
+                      Научитесь создавать современные сайты и веб-приложения
+                    </li>
+                    <li>
+                      Освойте HTML, CSS, JavaScript и получите навыки
+                      фронтенд-разработки
+                    </li>
+                    <li>Портфолио проектов уже во время обучения</li>
+                  </ul>
+                </div>
+                <div className="flex justify-between">
+                  <div className=" bg-[#F4F5FF] p-[15px] rounded-[30px] ">
+                    Срок обучения: 12 месяцев
+                  </div>
+                  <h1>Старт 3 декабря</h1>
+                </div>
               </div>
             </div>
           </div>
+          <h1 className="text-[22px] text-center cursor-pointer mt-[62px] bg-clip-text text-transparent bg-gradient-to-r from-[#1CBCEC] to-[#09147A]">
+            Смотреть больше
+          </h1>
         </div>
-        <div className="bg-[#E9EEFF]">
-          <div className="container px-[16px] mx-auto">
-            <div className="flex flex-wrap gap-[10px] justify-between p-[10px] mb-[120px] border border-[#0C1C35] rounded-lg mt-[60px]">
-              <Button className="text-[28px] p-[20px]" variant={"ghost"}>
-                Программирование
-              </Button>
-              <Button className="text-[28px]" size={"lg"}>
-                Дизайн
-              </Button>
-              <Button className="text-[28px]" variant={"ghost"} size={"lg"}>
-                Маркетинг
-              </Button>
-              <Button className="text-[28px]" variant={"ghost"}>
-                Аналитика
-              </Button>
+      </div>
+      <div>
+        <div className="container mx-auto px-[16px]">
+          <div className="flex py-[109px] px-[81px] bg-white rounded-[40px]">
+            <div className="flex flex-1 flex-col">
+              <h1 className="text-[36px]">Остались вопросы?</h1>
+              <span className="">
+                Если у вас остались вопросы, оставьте заявку - мы перезвоним
+              </span>
             </div>
-            <div className="flex gap-[25px]">
-              <div className="flex flex-col bg-background p-[23px] rounded-[45px]">
-                <Button className="mb-[60px] self-end" variant={"outline"}>
-                  Учиться
-                </Button>
-
-                <h1 className="mb-[90px] text-[#0C1C35] text-[28px]">
-                  Аналитик
-                </h1>
-                <span>Срок обучения 12 месяцев</span>
+            <div className="flex flex-1 flex-col space-y-3 gap-[26px]">
+              <Label>Имя</Label>
+              <Input placeholder="Введите имя" />
+              <div className="flex gap-[14px]">
+                <div className="flex flex-1 flex-col space-y-3">
+                  <Label>Телефон</Label>
+                  <Input placeholder="+7 |" />
+                </div>
+                <div className="flex flex-1 flex-col space-y-3">
+                  <Label>Электронная почта</Label>
+                  <Input type="email" placeholder="Введите email" />
+                </div>
               </div>
-              <div className="flex flex-col bg-background p-[23px] rounded-[45px]">
-                <Button className="mb-[60px] self-end" variant={"outline"}>
-                  Учиться
-                </Button>
-
-                <h1 className="mb-[90px] text-[#0C1C35] text-[28px]">
-                  Веб-дизайнер
-                </h1>
-                <span>Срок обучения 12 месяцев</span>
-              </div>
-              <div className="flex flex-col bg-background p-[23px] rounded-[45px]">
-                <Button className="mb-[60px] self-end" variant={"outline"}>
-                  Учиться
-                </Button>
-
-                <h1 className="mb-[90px] text-[#0C1C35] text-[28px]">
-                  Графический-дизайнер
-                </h1>
-                <span>Срок обучения 12 месяцев</span>
-              </div>
+              <h1 className="self-center">
+                Нажимая на кнопку, я соглашаюсь на обработку персональных данных
+              </h1>
+              <Button className="self-center w-1/2" variant={"outline"}>
+                Отправить
+              </Button>
             </div>
           </div>
         </div>
